a5281b6ea151cff84091049ba30799b35d63ea5c
Tsz Wai Levina Chan <chant8@cardiff.ac.uk>
2022-01-26 14:38:38 +0000

    forms radio field deleted style

--- blog/forms.py
+++ blog/forms.py
@@ -33,8 +33,7 @@ class CommentForm(FlaskForm):
 
 
 class RatingForm(FlaskForm):
-  style={'class': 'starstyle'}
-  star = RadioField('Rate this Post', validators=[InputRequired()], choices=[('1','1'),('2','2'),('3','3'),('4','4'),('5','5')], render_kw=style)
+  star = RadioField('Rate this Post', validators=[InputRequired()], choices=[('1','1'),('2','2'),('3','3'),('4','4'),('5','5')])
   submit = SubmitField('Rate')
 
   def validate_user_id(self, user_id, post_id, current_user):
